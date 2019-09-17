1ad3f44bd740247246839a723657082c06d1d1c2
Peter Chynoweth <pcchynoweth@alumni.uwaterloo.ca>
2019-09-17 11:56:03 -0600

    Create connection_info_live_test.php
    Initial commit

---
+++ connection_info_live_test.php
@@ -0,0 +1,8 @@
+<?php
+//*** VARIABLES ***//
+    /*    Database Username        */    $db_user = "username";
+    /*    Database Password        */    $db_pass = "password";
+    /*    Database Address         */    $db_host = "localhost";
+    /*    Database Name            */    $db_name = "dbname";
+
+?>
