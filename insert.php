59714771cdfef7a2a4a3603eef89c06c7e48820e
Peter Chynoweth <pcchynoweth@alumni.uwaterloo.ca>
2019-09-18 10:55:11 -0600

    Update for PDO
    Updated to use PDO interface to mysql

--- insert.php
+++ insert.php
@@ -1,7 +1,11 @@
 <?php  
- $connect = mysqli_connect("localhost", "username", "password", "dbname");  
+ require_once './connection_info_live_test.php';
+ 
+ $connect = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+ $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
  $sql = "INSERT INTO tbl_sample(first_name, last_name) VALUES('".$_POST["first_name"]."', '".$_POST["last_name"]."')";  
- if(mysqli_query($connect, $sql))  
+ if ($connect->exec($sql))  
  {  
       echo 'Data Inserted';  
  }  
