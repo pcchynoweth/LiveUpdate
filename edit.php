d0dc42185636969927b3c297a2fc290dca099af4
Peter Chynoweth <pcchynoweth@alumni.uwaterloo.ca>
2019-09-18 10:53:31 -0600

    Update to PDO
    Updated to use PDO interface to mysql

--- edit.php
+++ edit.php
@@ -1,10 +1,14 @@
 <?php  
- $connect = mysqli_connect("localhost", "username", "password", "dbname");  
+ require_once './connection_info_live_test.php';
+ 
+ $connect = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+ $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
  $id = $_POST["id"];  
  $text = $_POST["text"];  
  $column_name = $_POST["column_name"];  
  $sql = "UPDATE tbl_sample SET ".$column_name."='".$text."' WHERE id='".$id."'";  
- if(mysqli_query($connect, $sql))  
+ if ($connect->exec($sql))  
  {  
       echo 'Data Updated';  
  }  
