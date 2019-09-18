77aaed9470ebdf1e9be3e8005f0e108985019048
Peter Chynoweth <pcchynoweth@alumni.uwaterloo.ca>
2019-09-18 10:51:23 -0600

    Update to PDO
    Changed to PDO interface with mysql

--- delete.php
+++ delete.php
@@ -1,8 +1,12 @@
 <?php  
- $connect = mysqli_connect("localhost", "username", "password", "dbname");  
- $sql = "DELETE FROM tbl_sample WHERE id = '".$_POST["id"]."'";  
- if(mysqli_query($connect, $sql))  
- {  
+ require_once './connection_info_live_test.php';
+ 
+ $connect = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+ $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+  $sql = "DELETE FROM tbl_sample WHERE id = '".$_POST["id"]."'";  
+  if ($connect->exec($sql))  
+  {  
       echo 'Data Deleted';  
- }  
- ?>
+  }  
+?>
